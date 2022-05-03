6425440e3d20d9e36b11eab549581249c447df74
Francesco Valentini <Francesco.valentini@matteidigitale.org>
2022-05-03 17:37:53 +0200

    Inizio progettazione webapp

---
+++ webApp/index.php
@@ -0,0 +1,30 @@
+<html>
+    <head>
+        <link rel="stylesheet" href="css/index.css"> <!-- FOGLIO DI STILE INDEX -->
+        <title>HackerInside Notes</title>
+    </head>
+    <body>
+        <!-- HEADER -->
+        <div class="title">
+            <h1 style="font-size: 50px;">HackerInside Notes</h1>
+            <!-- <img src="img/logo.bmp" />-->
+        </div>
+        <!-- Barra di navigazione-->
+        <ul class="navBar">
+            <li><a class="active" href="index.html">HOME</a></li>
+            <li><a href="info.html">HackerInside Notes</a></li>
+            <li><a href="info.html">Contatti</a></li>
+        </ul>
+        <!-- Contenuto del sito -->
+        <div class="contentBox">
+            <p class="articolo">
+                Benvenuti in <b>HackerInside Notes</b><br />
+                <br />
+                Questo servizio completamente gratuito consente di prendere appunti e sincronizzarli attraverso il cloud!<br />
+                Dalla pagina dei downloads è possibile scaricare il client per android e per windows, per accedere alla web app cliccare su "HackerInside Notes"<br />
+            </p>
+            
+        </div>
+        
+    </body>
+</html>
